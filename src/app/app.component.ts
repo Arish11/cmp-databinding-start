72236205bc449db06f733752fc7a7f574b0c8f66
arish11 <abhishekarya.8596@gmail.com>
2023-06-30 15:31:23 +0530

    childToParent communication achieved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type:'server', name :'test', content :"test"},{type:"server", name :"test2", content :"test2"}]; 
+  serverElements = [{type:'server', name :'test', content :"test"},{type:"blueprint", name :"test2", content :"test2"}]; 
+
+  addItem(value:{type:string, name :string, content :string}){
+    this.serverElements.push({
+     type: value.type,
+     name: value.name,
+     content: value.content
+    })
+  }
 }
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
@@ -9,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class CockpitComponent implements OnInit {
   newServerName = '';
   newServerContent = '';
-
+  serverElements = []
+  @Output() newItemEvent = new EventEmitter<{type:string, name :string, content :string}>();
 
   constructor() { }
 
@@ -17,19 +18,31 @@ export class CockpitComponent implements OnInit {
   }
 
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+    this.newItemEvent.emit(this.serverElements.pop())
   }
 
-  onAddBlueprint() {
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
-  }
+  // onAddBlueprint() {
+  //   this.serverElements.push({
+  //     type: 'blueprint',
+  //     name: this.newServerName,
+  //     content: this.newServerContent
+  //   });
+  //   this.newItemEvent.emit(this.serverElements.pop())
+  // }
 
+
+  //You can either save the newly added server locally which is not recommended or
+  //you can implement the event emmiter directly like below. It will work in both the cases
+  onAddBlueprint(event){
+    this.newItemEvent.emit({ type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent})
+      console.log(event)
+  }
+  
 }
